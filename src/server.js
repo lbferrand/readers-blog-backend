71d9c0550ade9423d84aa9e73a9af348dec1df2c
lbferrand <lbferrand@gmail.com>
2023-06-07 21:11:23 -0400

    Add gitignore file and update server js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,53 @@
+import fs from "fs";
+import admin from "firebase-admin";
 import express from "express";
-import { db, connectToDb } from './db.js';
+import { db, connectToDb } from "./db.js";
 
+const credentials = JSON.parse(fs.readFileSync("./credentials.json"));
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/articles/:name', async (req, res) => {
-    const { name } = req.params;
+app.use(async (req, res, next) => {
+  const { authtoken } = req.headers;
 
-    const article = await db.collection('articles').findOne({ name });
-
-    if (article) {
-        res.json(article);
-    } else {
-        res.sendStatus(404);
+  if (authtoken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authtoken);
+    } catch (e) {
+      return res.sendStatus(400);
     }
+  }
 
+  req.user = req.user || {};
+
+  next();
+});
+
+app.get("/api/articles/:name", async (req, res) => {
+  const { name } = req.params;
+  const { uid } = req.user;
+
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.includes(uid);
     res.json(article);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 // app.post('/hello', (req, res) => {
@@ -29,43 +60,54 @@ app.get('/api/articles/:name', async (req, res) => {
 //     res.send(`Hello ${name}!!`);
 // })
 
-app.put('/api/articles/:name/upvote', async (req, res) => {
-    const { name } = req.params;
-    
-    await db.collection('articles').updateOne({ name }, { $inc: { upvotes: 1 },
-    });
-    const article = await db.collection('articles').findOne({ name });
-    
+app.put("/api/articles/:name/upvote", async (req, res) => {
+  const { name } = req.params;
+  const { uid } = req.user;
 
-    if (article) {
-        res.json(article);
-    } else {
-        res.send('That article doesn\'t exist');
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    const canUpvote = uid && !upvoteIds.includes(uid);
+
+    if (canUpvote) {
+      await db.collection("articles").updateOne(
+        { name },
+        {
+          $inc: { upvotes: 1 },
+          $push: { upvoteIds: uid },
+        }
+      );
     }
-    
-})
 
-app.post('/api/articles/:name/comments', async (req, res) => {
-    const { name } = req.params;
-    const { postedBy, text } = req.body;
+    const updatedArticle = await db.collection("articles").findOne({ name });
 
-    await db.collection('articles').updateOne({ name }, { $push: { comments: { postedBy, text } },
-    });
-    const article = await db.collection('articles').findOne({ name });
-
-    if (article) {
-        res.json(article);
-    } else {
-        res.send('That article doesn\'t exist!');
-    }
-    
+    res.json(updatedArticle);
+  } else {
+    res.send("That article doesn't exist");
+  }
 });
 
+app.post("/api/articles/:name/comments", async (req, res) => {
+  const { name } = req.params;
+  const { text } = req.body;
+  const { email } = req.user;
+
+  await db
+    .collection("articles")
+    .updateOne({ name }, { $push: { comments: { postedBy: email, text } } });
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    res.json(article);
+  } else {
+    res.send("That article doesn't exist!");
+  }
+});
 
 connectToDb(() => {
-    console.log('Successfully connected to database!');
-    app.listen(8000, () => {
-    console.log('Server is listening on port 5000');
+  console.log("Successfully connected to database!");
+  app.listen(5000, () => {
+    console.log("Server is listening on port 5000");
+  });
 });
-})
-
